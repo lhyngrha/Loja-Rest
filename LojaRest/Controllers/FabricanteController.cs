1068361f684b800743f3ab8aa54dc55ea3f29515
Gilson Gabriel Martins Diniz <gilson.gabriel@academico.ifrn.edu.br>
2016-06-30 17:13:14 -0300

    Acertou mizaravi

---
+++ LojaRest/Controllers/FabricanteController.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaRest.Controllers
+{
+    public class FabricanteController : ApiController
+    {
+        //Get api/fabricante - coloca no navegador para rodar o método
+        public IEnumerable<Models.Fabricante> Get()
+        {
+             Models.LojaDataContext dc = new Models.LojaDataContext();
+             var r = from f in dc.Fabricantes select f;
+             return r.ToList();
+        }
+
+        public void Post([FromBody] string value)
+        {
+             List<Models.Fabricante> x = JsonConvert.DeserializeObject<List< Models.Fabricante >> (value);
+             Models.LojaDataContext dc = new Models.LojaDataContext();
+             dc.Fabricantes.InsertAllOnSubmit(x);
+             dc.SubmitChanges();
+         }
+
+        public void Put(int id, [FromBody] string value)
+        {
+             Models.Fabricante x = JsonConvert.DeserializeObject< Models.Fabricante > (value);
+             Models.LojaDataContext dc = new Models.LojaDataContext();
+             Models.Fabricante fab = (from f in dc.Fabricantes
+             where f.Id == id select f).Single();
+             fab.Descricao = x.Descricao;
+             dc.SubmitChanges();
+        }
+
+         public void Delete(int id)
+        {
+             Models.LojaDataContext dc = new Models.LojaDataContext();
+             Models.Fabricante fab = (from f in dc.Fabricantes
+             where f.Id == id select f).Single();
+             dc.Fabricantes.DeleteOnSubmit(fab);
+             dc.SubmitChanges();
+         }
+    }
+}
---
+++ LojaRest/Controllers/VeiculoController.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaRest.Controllers
+{
+    public class VeiculoController : ApiController
+    {
+        public IEnumerable<Models.Veiculo> Get()
+        {
+            Models.LojaDataContext dc = new Models.LojaDataContext();
+            var r = from f in dc.Veiculos select f;
+            return r.ToList();
+        }
+
+        public void Post([FromBody] string value)
+        {
+            List<Models.Veiculo> x = JsonConvert.DeserializeObject<List<Models.Veiculo>>(value);
+            Models.LojaDataContext dc = new Models.LojaDataContext();
+            dc.Veiculos.InsertAllOnSubmit(x);
+            dc.SubmitChanges();
+        }
+
+        public void Put(int id, [FromBody] string value)
+        {
+            Models.Veiculo x = JsonConvert.DeserializeObject<Models.Veiculo>(value);
+            Models.LojaDataContext dc = new Models.LojaDataContext();
+            Models.Veiculo vei = (from f in dc.Veiculos
+                                     where f.Id == id
+                                     select f).Single();
+            vei.Modelo = x.Modelo;
+            vei.IdFabricante = x.IdFabricante;
+            dc.SubmitChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Models.LojaDataContext dc = new Models.LojaDataContext();
+            Models.Veiculo vei = (from f in dc.Veiculos
+                                     where f.Id == id
+                                     select f).Single();
+            dc.Veiculos.DeleteOnSubmit(vei);
+            dc.SubmitChanges();
+        }
+    }
+}
